857f3874efcd869be238f52f41981b85a5dfb2f5
robertszekely <robert.szekely@halcyonmobile.com>
2019-09-07 19:51:00 +0300

    Implement flexible update and add logs + toasts to monitor the process

--- app/src/main/java/com/example/updateme/MainActivity.kt
+++ app/src/main/java/com/example/updateme/MainActivity.kt
@@ -5,36 +5,75 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.play.core.appupdate.AppUpdateInfo
 import com.google.android.play.core.appupdate.AppUpdateManager
 import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.install.InstallState
+import com.google.android.play.core.install.InstallStateUpdatedListener
 import com.google.android.play.core.install.model.ActivityResult
 import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.InstallStatus
 import com.google.android.play.core.install.model.UpdateAvailability
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appUpdateManager: AppUpdateManager
+    private lateinit var installStateUpdatedListener: InstallStateUpdatedListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         appUpdateManager = AppUpdateManagerFactory.create(this)
+
+        installStateUpdatedListener = InstallStateUpdatedListener { state ->
+            handleInstallState(state)
+        }
+
+        appUpdateManager.registerListener(installStateUpdatedListener)
+
         checkForUpdate()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == REQUEST_CODE_FLEXIBLE_UPDATE) {
+            // onActivityResult triggered after calling startUpdateFlowForResult()
             when (resultCode) {
                 Activity.RESULT_OK -> {
-                }//  handle user's approval
+                    // handle user's approval
+                    showToastAndLogMessage("User accepted the update")
+                }
                 Activity.RESULT_CANCELED -> {
-                }//  handle user's rejection
+                    // handle user's rejection
+                    showToastAndLogMessage("User cancelled the update")
+                    checkForUpdate()
+                }
                 ActivityResult.RESULT_IN_APP_UPDATE_FAILED -> {
-                } //  handle update failure
+                    // handle update failure
+                    showToastAndLogMessage("Update failed")
+                    checkForUpdate()
+                }
             }
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        appUpdateManager.unregisterListener(installStateUpdatedListener)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        //check if the update downloaded while the app was in the background
+        appUpdateManager.appUpdateInfo
+            .addOnSuccessListener { appUpdateInfo ->
+                if (appUpdateInfo.installStatus() == InstallStatus.DOWNLOADED) {
+                    showToastAndLogMessage("Update was downloaded in the background")
+                    appUpdateManager.completeUpdate()
+                }
+            }
     }
 
     private fun requestUpdate(appUpdateInfo: AppUpdateInfo?) {
@@ -59,7 +98,26 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun showToastAndLogMessage(message: String){
+    private fun handleInstallState(state: InstallState) {
+        if (state.installStatus() == InstallStatus.DOWNLOADED) {
+            // After the update is downloaded, show a notification
+            // and request user confirmation to restart the app.
+            Log.d(MainActivity::class.java.simpleName, "Updated downloaded.")
+            popupSnackbarForCompleteUpdate()
+        }
+    }
+
+    private fun popupSnackbarForCompleteUpdate() {
+        Snackbar.make(
+            findViewById(R.id.activity_main_layout), "An update has just been downloaded.", Snackbar.LENGTH_INDEFINITE
+        ).apply {
+            setAction("RESTART") { appUpdateManager.completeUpdate() }
+            setActionTextColor(ContextCompat.getColor(this@MainActivity, R.color.white))
+            show()
+        }
+    }
+
+    private fun showToastAndLogMessage(message: String) {
         showToast(message)
         Log.d(MainActivity::class.java.simpleName, message)
     }
